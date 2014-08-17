3da8b3d771d094f587d2abd950fe336bf7e6db3d
Satgo1546 <satgo1546@yeah.net>
2014-08-17 13:56:18 +0800

    添加REXML库

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@
 # Written by satgo1546
 # 适配于符合XML标准的HTML文件。会忽略DOCTYPE。
 
+require 'rexml/document'
+
 module HTML2Discuz
 	GENERATOR_SIGNATURE = <<ENDBB.chomp
 [align=right][font=Microsoft YaHei][size=10pt][color=Silver]此帖子的代码由[b]HTML → Discuz!代码转换器[/b]生成。[/color][/size][/font][/align]
@@ -12,6 +14,7 @@ ENDBB
 		c = File.read(from,	:encoding => "utf-8")
 		c.gsub!(/<!DOCTYPE.*?>/i, "")
 		# --------------------------------------------------------------------------
+		
 		# --------------------------------------------------------------------------
 		c.chomp!
 		c << GENERATOR_SIGNATURE
