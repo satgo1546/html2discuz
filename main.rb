be38a5f9efb6b48238804b4a459d14961e098386
Satgo1546 <satgo1546@yeah.net>
2014-08-17 14:15:52 +0800

    添加Processor

--- main.rb
+++ main.rb
@@ -4,8 +4,36 @@
 # 适配于符合XML标准的HTML文件。会忽略DOCTYPE。
 
 require 'rexml/document'
+require 'rexml/streamlistener'
 
 module HTML2Discuz
+	class Processor
+		IGNORED_TAGS = [
+			:html,
+			:head,
+			:body,
+			:meta
+		]
+		include REXML::StreamListener
+		attr_reader :result
+		def initialize
+			clear_result
+		end
+		def clear_result
+			@result = ""
+		end
+		def tag_start(name, attributes)
+			return if IGNORED_TAGS.include?(name.to_sym)
+			@result << "[#{name}]"
+		end
+		def tag_end(name)
+			return if IGNORED_TAGS.include?(name.to_sym)
+			@result << "[/#{name}]"
+		end
+		def text(text)
+			@result << text
+		end
+	end
 	GENERATOR_SIGNATURE = <<ENDBB.chomp
 [align=right][font=Microsoft YaHei][size=10pt][color=Silver]此帖子的代码由[b]HTML → Discuz!代码转换器[/b]生成。[/color][/size][/font][/align]
 ENDBB
@@ -13,12 +41,11 @@ ENDBB
 		quiet = options.include?("--quiet")
 		c = File.read(from,	:encoding => "utf-8")
 		c.gsub!(/<!DOCTYPE.*?>/i, "")
-		# --------------------------------------------------------------------------
-		
-		# --------------------------------------------------------------------------
-		c.chomp!
-		c << GENERATOR_SIGNATURE
-		File.write(to, c)
+		processor = Processor.new
+		rx = REXML::Parsers::StreamParser.new(c, processor)
+		rx.parse
+		result = processor.result.chomp + GENERATOR_SIGNATURE
+		File.write(to, result)
 	end
 end
 
